903f81f91c29c01cf577046dbea2c45bf8b9276c
LoneDev6 <leoxy98@gmail.com>
2024-01-24 12:20:11 +0100

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,80 +1,50 @@
 import React from 'react';
-import clsx from 'clsx';
-import Layout from '@theme/Layout';
-import Link from '@docusaurus/Link';
-import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
-import styles from './index.module.css';
-import HomepageFeatures from '../components/HomepageFeatures';
-import  { Redirect } from 'react-router-dom';
-
-function HomepageHeader() {
-  const {siteConfig} = useDocusaurusContext();
-  return (
-    <header className={clsx('hero', styles.heroBanner)}>
-      <div className="container">
-        <h1 className="hero__title">{siteConfig.title}</h1>
-        <p className="hero__subtitle">{siteConfig.tagline}</p>
-        <div className={styles.buttons}>
-          <Link
-            className="button button--secondary button--lg"
-            to="/blog/">
-            Enter the blog
-          </Link>
-        </div>
-      </div>
-    </header>
-  );
-}
+import BrowserOnly from '@docusaurus/BrowserOnly';
 
 export default function Home() {
-  // const {siteConfig} = useDocusaurusContext();
-  // return (
-  //   <Layout
-  //     title={`Hello from ${siteConfig.title}`}
-  //     description="Personal blog of LoneDev">
-  //     <HomepageHeader />
-  //   </Layout>
-  // );
-  return (<Redirect to='/blog' />);
+  return (
+    <BrowserOnly>
+      {(function() {
+
+        // Shit to redirect to blog.
+        window.location.href = "/blog";
+
+        // Change these if you use something different in your hook.
+        var storageKey = 'darkMode';
+        var classNameDark = 'dark-mode';
+        var classNameLight = 'light-mode';
+
+        function setClassOnDocumentBody(darkMode) {
+          document.body.classList.add(darkMode ? classNameDark : classNameLight);
+          document.body.classList.remove(darkMode ? classNameLight : classNameDark);
+        }
+        
+        var preferDarkQuery = '(prefers-color-scheme: dark)';
+        var mql = window.matchMedia(preferDarkQuery);
+        var supportsColorSchemeQuery = mql.media === preferDarkQuery;
+        var localStorageTheme = null;
+        try {
+          localStorageTheme = localStorage.getItem(storageKey);
+        } catch (err) {}
+        var localStorageExists = localStorageTheme !== null;
+        if (localStorageExists) {
+          localStorageTheme = JSON.parse(localStorageTheme);
+        }
+
+        // Determine the source of truth
+        if (localStorageExists) {
+          // source of truth from localStorage
+          setClassOnDocumentBody(localStorageTheme);
+        } else if (supportsColorSchemeQuery) {
+          // source of truth from system
+          setClassOnDocumentBody(mql.matches);
+          localStorage.setItem(storageKey, mql.matches);
+        } else {
+          // source of truth from document.body
+          var isDarkMode = document.body.classList.contains(classNameDark);
+          localStorage.setItem(storageKey, JSON.stringify(isDarkMode));
+        }
+      })()}
+    </BrowserOnly>
+  );
 }
-
-// Insert this script in your index.html right after the <body> tag.
-// This will help to prevent a flash if dark mode is the default.
-
-(function() {
-  // Change these if you use something different in your hook.
-  var storageKey = 'darkMode';
-  var classNameDark = 'dark-mode';
-  var classNameLight = 'light-mode';
-
-  function setClassOnDocumentBody(darkMode) {
-    document.body.classList.add(darkMode ? classNameDark : classNameLight);
-    document.body.classList.remove(darkMode ? classNameLight : classNameDark);
-  }
-  
-  var preferDarkQuery = '(prefers-color-scheme: dark)';
-  var mql = window.matchMedia(preferDarkQuery);
-  var supportsColorSchemeQuery = mql.media === preferDarkQuery;
-  var localStorageTheme = null;
-  try {
-    localStorageTheme = localStorage.getItem(storageKey);
-  } catch (err) {}
-  var localStorageExists = localStorageTheme !== null;
-  if (localStorageExists) {
-    localStorageTheme = JSON.parse(localStorageTheme);
-  }
-
-  // Determine the source of truth
-  if (localStorageExists) {
-    // source of truth from localStorage
-    setClassOnDocumentBody(localStorageTheme);
-  } else if (supportsColorSchemeQuery) {
-    // source of truth from system
-    setClassOnDocumentBody(mql.matches);
-    localStorage.setItem(storageKey, mql.matches);
-  } else {
-    // source of truth from document.body
-    var isDarkMode = document.body.classList.contains(classNameDark);
-    localStorage.setItem(storageKey, JSON.stringify(isDarkMode));
-  }
-})();
